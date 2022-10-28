ce67652edf754fcc21b5baced926525984c87cf5
Ayobams108 <oyedokunayobami8@gmail.com>
2022-10-28 00:24:57 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
-* set_bit - sets the value of a bit to 1 at a given index.
+* get_bit - Returns the value of a bit at a given index.
 * @n: number
 * @index: index
-* Return: change bit in index
+* Return: bit in index
 */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int t_binary;
 
-	t_binary = (sizeof(*(n)) * 8);
+	t_binary = (sizeof(n) * 8);
 	if (index < t_binary)
 	{
-		*(n) = ((1 << index) | *(n));
-		return (1);
+		return (((1 << index) & n) >> index);
 	}
 	return (-1);
-
-
 }
